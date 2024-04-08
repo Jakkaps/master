92d3e55bdcffc73a43cb721ecc59918dd6223128
Jens <jensakrr@gmail.com>
2024-04-08 09:11:08 +0200

    Fix some things on data preprocessing

--- chat_dataset.py
+++ chat_dataset.py
@@ -15,21 +15,20 @@ class ChatDataset(InMemoryDataset):
         return [f"{self.dataset}_processed.pt"]
 
     def process(self):
-        node_encodings = torch.load(f"{self.root}/{self.dataset}_node_encodings.pt")
+        nodes = torch.load(f"{self.root}/{self.dataset}_nodes.pt")
         edge_idxs = torch.load(f"{self.root}/{self.dataset}_edge_idxs.pt")
         edges = torch.load(f"{self.root}/{self.dataset}_edges.pt")
         labels = torch.load(f"{self.root}/{self.dataset}_labels.pt")
 
         data_list = [
             Data(
-                x=node_encoding,
+                x=node,
                 edge_index=edge_idx,
                 edge_attr=edge,
                 y=label,
+                num_nodes=len(node),
             )
-            for node_encoding, edge_idx, edge, label in zip(
-                node_encodings, edge_idxs, edges, labels
-            )
+            for node, edge_idx, edge, label in zip(nodes, edge_idxs, edges, labels)
         ]
 
         self.save(data_list, self.processed_paths[0])
