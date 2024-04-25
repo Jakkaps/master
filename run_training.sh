5885c37196e5125ebaae07ab2f8a979bd65414c4
Jens <jensakrr@gmail.com>
2024-04-25 15:12:47 +0200

    Change batch size to 16

--- run_training.sh
+++ run_training.sh
@@ -9,9 +9,8 @@
 #SBATCH --constraint=gpu16g
 #SBATCH --output=training.txt
 
--
 
 module load Python/3.9.6-GCCcore-11.2.0
 module load CUDA/11.8.0
 source .venv/bin/activate
-python run_training.py --lr 0.001 --epochs 5 --batch_size 32
+python run_training.py --lr 0.001 --epochs 5 --batch_size 16
