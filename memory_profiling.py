58d285b0e88f80f7418496ef00b30398a9504a58
Jens <jensakrr@gmail.com>
2024-05-09 18:59:25 +0200

    Benchmark w larger batch sizes

--- memory_profiling.py
+++ memory_profiling.py
@@ -19,7 +19,7 @@ criterion = HingeLoss()
 dataset = DialogDiscriminationDataset(
     root="data/twitter_cs", dataset="twitter_cs", split="train"
 )
-loader = DataLoader(dataset, batch_size=1, shuffle=True)
+loader = DataLoader(dataset, batch_size=25, shuffle=True)
 input_data = next(iter(loader)).to(device)
 target = input_data.y
 
@@ -27,7 +27,7 @@ scaler = GradScaler()
 
 
 # Function to measure memory usage
-def check_memory_usage():
+def check_mem_usage_mixed_precision():
     torch.cuda.empty_cache()
     torch.cuda.reset_peak_memory_stats()
 
@@ -49,5 +49,27 @@ def check_memory_usage():
     print(f"Current memory usage: {current_memory:.2f} MB")
 
 
+def check_mem_usage():
+    torch.cuda.empty_cache()
+    output = model(input_data)
+    loss = criterion(output, target)
+
+    # Backward pass
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    # Retrieve the memory stats
+    peak_memory = torch.cuda.max_memory_allocated(device) / (1024**2)  # Convert to MB
+    current_memory = torch.cuda.memory_allocated(device) / (1024**2)  # Convert to MB
+
+    print(f"Peak memory usage: {peak_memory:.2f} MB")
+    print(f"Current memory usage: {current_memory:.2f} MB")
+
+
 # Call the function to check memory usage
-check_memory_usage()
+print("With mixed precision:")
+check_mem_usage_mixed_precision()
+
+print("\nWithout mixed precision:")
+check_mem_usage()
